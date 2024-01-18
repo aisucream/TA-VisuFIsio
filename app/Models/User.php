13c2164510a4b4c5f5092d73f6fdd0ce14ddcfc7
aisucream <arcadiusobaja@gmail.com>
2024-01-18 18:51:14 +0700

    Revisi1

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,9 +9,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+    
+
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable,SoftDeletes;
+
 
     /**
      * The attributes that are mass assignable.
